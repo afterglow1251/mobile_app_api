afc4da87ac61fce05e8521df121e62abfd70e64b
Merciless <yurii.afterglow@gmail.com>
2024-12-22 14:20:53 +0200

    fix previous commit

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -81,11 +81,7 @@ export class OrdersService {
   async getAllOrders(userId: number) {
     const orders = await this.ordersRepository.find({
       where: { user: { id: userId } },
-      relations: [
-        'orderItems',
-        'orderItems.product',
-        'orderItems.product.images',
-      ],
+      relations: ['orderItems.product.images'],
       order: { createdAt: 'DESC' },
     });
 
